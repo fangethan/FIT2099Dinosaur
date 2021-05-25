0fc9668496ea3d9052d1384452df0df4335d709e
Cleshan <cwar0007@student.monash.edu>
2021-05-25 19:01:54 +1000

    Fixed small bug about exiting game

--- src/game/Application.java
+++ src/game/Application.java
@@ -79,19 +79,24 @@ public class Application {
 				"...............................+++++............................................",
 				".................................+..............................................",
 				"................................................................................");
-		//Adding new game map
-		GameMap gameMap = new GameMap(groundFactory, map );
+		//Adding new game
+
+		if (world == null) {
+			System.exit(0);
+		}
+
+		GameMap gameMap = new GameMap(groundFactory, map);
 		world.addGameMap(gameMap);
 
 		GameMap newGameMap = new GameMap(groundFactory, newMap);
 		world.addGameMap(newGameMap);
 
 		NumberRange w = gameMap.getXRange();
-		NumberRange h= gameMap.getYRange();
+		NumberRange h = gameMap.getYRange();
 		int max_height = h.max();
 		int min_height = h.min();
 		// Adding access to the other map
-		for (int i:w){
+		for (int i : w) {
 			Location topMap1 = gameMap.at(i, h.min());
 			topMap1.addExit(new Exit("next map", newGameMap.at(i, max_height), "8"));
 		}
@@ -107,19 +112,19 @@ public class Application {
 		world.addPlayer(player, gameMap.at(9, 4));
 
 		// Place a pair of stegosaurs in the middle of the map
-		Stegosaur male = new Stegosaur("Stegosaur",'M');
-		Stegosaur female = new Stegosaur("Stegosaur",'F');
+		Stegosaur male = new Stegosaur("Stegosaur", 'M');
+		Stegosaur female = new Stegosaur("Stegosaur", 'F');
 
 		//Places 3 brachisaurs on the map
-		Brachiosaur brachiosaur1 = new Brachiosaur("Brachiosaur",'M');
-		Brachiosaur brachiosaur2 = new Brachiosaur("Brachiosaur",'F');
+		Brachiosaur brachiosaur1 = new Brachiosaur("Brachiosaur", 'M');
+		Brachiosaur brachiosaur2 = new Brachiosaur("Brachiosaur", 'F');
 
 		//Places 3 allosaurs on the map
 		Allosaur allosaur1 = new Allosaur("Allosaur", 'M');
 
 		Pterodactyls pterodactyl1 = new Pterodactyls("Pterodactyl", 'M');
-		Pterodactyls pterodactyl2 = new Pterodactyls("Pterodactyl",'F');
-		Pterodactyls pterodactyl3 = new Pterodactyls("Pterodactyl",'F');
+		Pterodactyls pterodactyl2 = new Pterodactyls("Pterodactyl", 'F');
+		Pterodactyls pterodactyl3 = new Pterodactyls("Pterodactyl", 'F');
 
 		gameMap.at(10, 12).addActor(pterodactyl1);
 		gameMap.at(2, 2).addActor(pterodactyl2);
@@ -132,7 +137,7 @@ public class Application {
 		gameMap.at(10, 20).addActor(male);
 		gameMap.at(30, 10).addActor(female);
 
-		gameMap.at(64,10).addActor(allosaur1);
+		gameMap.at(64, 10).addActor(allosaur1);
 
 		// for testing purposes
 //		Corpse corpse = new Corpse(female.getDisplayChar());
@@ -141,14 +146,14 @@ public class Application {
 
 		NumberRange widths = gameMap.getXRange();
 		NumberRange height = gameMap.getYRange();
-		gameMap.at(5,7).setGround(new VendingMachine());
+		gameMap.at(5, 7).setGround(new VendingMachine());
 
 		// adding fish to the lake
-		for (int x:widths){
-			for (int y:height) {
+		for (int x : widths) {
+			for (int y : height) {
 				Location location = gameMap.at(x, y);
 				if (location.getGround() instanceof Lake) {
-					for (int i =0; i<5; i++){
+					for (int i = 0; i < 5; i++) {
 						location.addItem(new Fish());
 					}
 
@@ -156,9 +161,14 @@ public class Application {
 			}
 		}
 
-		gameMap.at(5,7).setGround(new VendingMachine());
+		gameMap.at(5, 7).setGround(new VendingMachine());
 
-		world.run();
+
+
+			world.run();
+
 	}
+
+
 
 }
